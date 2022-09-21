7959f3a6dcdeb71a7424554a301b0e63eb682d14
Camila <camilagbortolotti@gmail.com>
2022-09-20 21:13:07 -0300

    Adiciona livros ao banco de dados

--- routes/booksBp.py
+++ routes/booksBp.py
@@ -1,5 +1,6 @@
 from urllib.parse import _ResultMixinStr
 from flask import Blueprint, render_template
+from flask import Flask, escape, request
 
 from ..models.reading import Reading
 from ..extensions import db
@@ -27,9 +28,19 @@ def reading_list():
 
     return render_template('books.html', books = result, title='Reading')
 
-@booksBp.route('/insertbook')
-def insertbook_list():
+@booksBp.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        form = request.form
+        newBook = Books(name = form['InputBook_Name'], writer = form['InputWrite_Name'], 
+        genre = form['InputGenre_Name'], release = form['InputRelease_Name'], 
+        pages = form['InputPages_Name'], publisher = form['InputPublisher_Name'])
+        
+        db.session.add(newBook)   
+        db.session.commit()
 
-    return render_template('insertbook.html')
-    
+        # for key, value in request.form.items():
+        #     print(f'{key}: {value}')
+
+    return render_template('insertbook.html', title='Insert New Books')
     
